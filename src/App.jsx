2c79146aa4503cc4926697c55b40059f140b3657
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-11 22:13:02 +0300

    chore: add docs, get some general setup before starting on working on a project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1 className="">Your Shopping Cart</h1>
+      <div className="min-h-screen bg-gray-100 w-full">
+        <div className="grid grid-cols-2 gap-6 w-full max-w-4xl mx-auto p-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="card col-span-1 aspect-square"></div>
+            <div className="card col-span-1 aspect-square"></div>
+            <div className="card col-span-1 aspect-square"></div>
+            <div className="card col-span-1 aspect-square"></div>
+          </div>
+          <div className="card"></div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/ShopItems.js
@@ -0,0 +1,12 @@
+export const groceryItems = [
+  { name: "Apples", quantity: 3, price: 2.99 },
+  { name: "Bananas", quantity: 5, price: 1.99 },
+  { name: "Bread", quantity: 1, price: 3.49 },
+  { name: "Milk", quantity: 1, price: 2.99 },
+  { name: "Eggs", quantity: 12, price: 4.99 },
+  { name: "Cheese", quantity: 1, price: 5.99 },
+  { name: "Chicken", quantity: 2, price: 8.99 },
+  { name: "Beef", quantity: 1, price: 12.99 },
+  { name: "Pasta", quantity: 2, price: 1.99 },
+  { name: "Rice", quantity: 1, price: 3.99 },
+];
