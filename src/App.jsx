eb7bbd2c97268ca760f001696f0307162d638cbe
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-11 22:51:39 +0300

    feat: start working on items section, import all of the items from dictionary. still trying to figure out the pictures though

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="min-h-screen bg-gray-100 w-full">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full max-w-4xl mx-auto p-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full max-w-5xl mx-auto p-4">
           <Items />
           <div className="card"></div>
         </div>
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -9,11 +9,28 @@ const Items = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         {ShopItems.map((item) => (
           <div className="card" key={item.id}>
-            <img src={"../assets/" + item.image} alt={item.name} />
-            <div className="card-body">
-              <h2>{item.name}</h2>
-              <p>{item.price}</p>
-              <button>Add to Cart</button>
+            <div className="w-full bg-blue-400 rounded-lg aspect-square"></div>
+            {/* <img src={"../assets/" + item.image} alt={item.name} /> */}
+            <div className="flex flex-col">
+              <div className="flex flex-col mt-2">
+                <h2 className="font-medium text-lg">{item.name}</h2>
+                <p className="text-slate-700">${item.price}</p>
+              </div>
+
+              <div className="grid grid-cols-2 items-center mt-2">
+                <div className="flex flex-row gap-2 items-center">
+                  <button className="rounded-full bg-blue-100 w-6 h-6">
+                    -
+                  </button>
+                  <p className="font-medium">{item.quantity}</p>
+                  <button className="rounded-full bg-blue-100 w-6 h-6">
+                    +
+                  </button>
+                </div>
+                <button className="rounded-lg bg-blue-400 hover:bg-blue-500 active:scale-95 duration-500 w-full p-1 font-medium text-white">
+                  Add to Cart
+                </button>
+              </div>
             </div>
           </div>
         ))}
