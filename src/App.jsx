95999b7dc95af006506092f977b1d781ccf83199
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-12 12:05:21 +0300

    refactor: restart the app from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
-import Items from "./components/Items";
 
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-gray-100 w-full">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full max-w-5xl mx-auto p-4">
-          <Items />
-          <div className="card"></div>
-        </div>
-      </div>
+      <div className="flex"></div>
     </>
   );
 }
