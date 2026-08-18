d7d6780b061e9e40becc9e6cb90d94ff1d29ef45
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-18 15:33:56 +0300

    feat: add an empty next project

---
+++ client/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Shopping Cart App",
+  description: "This is a simple proof of concept shopping cart app.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,8 @@
+export default function IndexPage() {
+  return (
+    <div className="min-h-screen overflow-hidden flex">
+      <aside className="w-60 p-3 h-screen fixed border-r-2">Amen</aside>
+      <main className="flex-1 overflow-y-auto ml-60 p-3"></main>
+    </div>
+  );
+}
