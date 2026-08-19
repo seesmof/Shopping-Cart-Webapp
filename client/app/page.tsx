6904e39d308ee05b91e00ed1c48c18b71b5634bf
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-19 11:10:33 +0300

    feat: add a badge showing how many items are in a cart

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,15 +3,20 @@ import Link from "next/link";
 
 export default function IndexPage() {
   return (
-    <div className="min-h-">
-      <div className="sticky top-0 z-50 bg-white border-b-2">
+    <div className="min-h-screen">
+      <nav className="sticky top-0 z-40 bg-white border-b-2">
         <div className="max-w-md mx-auto p-3 flex justify-between items-center">
-          <h1 className="font-bold">
+          <h1 className="font-bold hover:underline underline-offset-4">
             <Link href={"/"}>CartApp</Link>
           </h1>
-          <Button>Cart</Button>
+          <Button className="relative">
+            <p>Cart</p>
+            <div className="absolute -top-1 -right-1 z-50 rounded-full bg-sky-500 w-4 h-4 flex items-center justify-center">
+              0
+            </div>
+          </Button>
         </div>
-      </div>
+      </nav>
     </div>
   );
 }
