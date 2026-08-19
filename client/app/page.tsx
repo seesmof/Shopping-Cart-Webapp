e2aaa9b987e002abd671b3ca09027d5f0c0b6bc3
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-19 11:04:19 +0300

    feat: add a navbar

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function IndexPage() {
-  return <div className="min-h-screen overflow-hidden flex"></div>;
+  return (
+    <div className="min-h-">
+      <div className="sticky top-0 z-50 bg-white border-b-2">
+        <div className="max-w-md mx-auto p-3 flex justify-between items-center">
+          <h1 className="font-bold">
+            <Link href={"/"}>CartApp</Link>
+          </h1>
+          <Button>Cart</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
